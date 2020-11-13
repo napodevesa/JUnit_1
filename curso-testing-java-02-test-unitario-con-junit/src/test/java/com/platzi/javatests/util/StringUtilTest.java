2c7090f9061bf2cc2cdbf468d15d82d10e2692cb
ndev <napodevesa@gmail.com>
2020-11-13 14:31:57 +0100

    assert

--- curso-testing-java-02-test-unitario-con-junit/src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ curso-testing-java-02-test-unitario-con-junit/src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -10,5 +10,6 @@ public class StringUtilTest {
 
         Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
         Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+        Assert.assertEquals("holahola", StringUtil.repeat("hola", 2));
     }
 }
