ecc210957551916eda69512968749161dd841f41
ndev <napodevesa@gmail.com>
2020-11-13 14:08:56 +0100

    2

---
+++ src/test/java/com/platzi/javatests/util/PasswordUtil.java
@@ -0,0 +1,13 @@
+package com.platzi.javatests.util;
+
+public class PasswordUtil {
+
+    public enum SecurityLevel{
+        WEAK, MEDIUM, STRONG
+    }
+
+    public static SecurityLevel assessPassword(String password){
+
+      return null;
+    }
+}
