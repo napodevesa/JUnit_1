33475808167575eb948c6f2908cc3c73eb16b428
ndev <napodevesa@gmail.com>
2020-11-13 14:41:49 +0100

    Test organization  with JUnit

--- curso-testing-java-02-test-unitario-con-junit/src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ curso-testing-java-02-test-unitario-con-junit/src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -6,10 +6,23 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
+    public void repeat_string_once() {
+
+        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+    }
+    @Test
+    public void repeat_string_multiple_times() {
 
         Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
-        Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
         Assert.assertEquals("holahola", StringUtil.repeat("hola", 2));
+    }
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola", -1);
     }
 }
